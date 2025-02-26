bce9d20eda50abad255224916c4d0beb8abdbac1
andreandradedp <andreandradedp@gmail.com>
2025-02-26 15:16:45 +0000

    Adicionando arquivos iniciais

---
+++ src/services/firebase.js
@@ -0,0 +1,20 @@
+// src/services/firebase.js
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_AUTH_DOMAIN",
+  projectId: "SEU_PROJECT_ID",
+  storageBucket: "SEU_STORAGE_BUCKET",
+  messagingSenderId: "SEU_MESSAGING_SENDER_ID",
+  appId: "SEU_APP_ID"
+};
+
+// Inicializa o aplicativo Firebase
+const app = initializeApp(firebaseConfig);
+
+// Exporta o Firestore e o Auth para uso nos componentes
+export const db = getFirestore(app);
+export const auth = getAuth(app);
